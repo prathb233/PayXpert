2c9f50780f6911722ab0c5fff2c2bee48857d08b
Pratham Bhoite <69412716+prathb233@users.noreply.github.com>
2024-02-08 02:13:13 +0530

    Update Main.java
    changes in comment

--- src/main/java/com/hexaware/payxpert/main/Main.java
+++ src/main/java/com/hexaware/payxpert/main/Main.java
@@ -47,7 +47,7 @@ public class Main {
 	}
 	
 	/**
-	 * This is method is Main Menu which displays all the functions which can be performed
+	 * This method is the Main Menu which displays all the functions which can be performed in this application
 	 * @param scanner to reduce code redundancy we are creating scanner instance only once and passing it to every method which requires input
 	 */
     public static void displayMenu(Scanner scanner){
