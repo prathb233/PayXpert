36c1d0df0396c2a1604c3e8820fbbc5a2d9c458b
Pratham Bhoite <prathb233@gmail.com>
2024-02-11 22:42:13 +0530

    changes in test

--- src/test/java/com/hexaware/payxpert/dao/PayrollDAOTest.java
+++ src/test/java/com/hexaware/payxpert/dao/PayrollDAOTest.java
@@ -2,6 +2,7 @@ package com.hexaware.payxpert.dao;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,12 +11,11 @@ import com.hexaware.payxpert.util.DBConnection;
 
 public class PayrollDAOTest extends DBConnection{
 	
-    private PayrollDAO payrollDAO;
+    private PayrollDAO payrollDAO = new PayrollDAO();
 
     @Before
     public void setUp() {
     	//establishing connection with DB before testing
-    	payrollDAO = new PayrollDAO();
     	payrollDAO.getConn();
     }
     
@@ -23,8 +23,8 @@ public class PayrollDAOTest extends DBConnection{
     public void testGetPayrollById() {
         // Creating test variables to compare with actual values
         int testPayrollId = 1001;
-        double expectedGrossSalary = 77900.0;
-        double expectedNetSalary = 75900.0;
+        double expectedGrossSalary = 92530.0;
+        double expectedNetSalary = 87930.0;
 
         // Call the method to get the actual values.
         Payroll payroll = payrollDAO.getPayrollById(testPayrollId);
@@ -32,6 +32,12 @@ public class PayrollDAOTest extends DBConnection{
         // Assert that the actual values match the expected values.
         assertEquals(expectedGrossSalary, payroll.getGrossSalary(), 0.001);
         assertEquals(expectedNetSalary, payroll.getNetSalary(), 0.001); // 0.001 is the delta for double comparison.
+    }
+    
+    @After
+    public void closeSetUp() {
+    	//closing the connection
+    	payrollDAO.callCloseCon();
     }
 
 }
